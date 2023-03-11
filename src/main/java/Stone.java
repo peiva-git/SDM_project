963c1dd2bc8c1c04bf68f3c1864e9b487fc1e9ab
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-11 22:03:28 +0100

    Override the equals method

--- src/main/java/Stone.java
+++ src/main/java/Stone.java
@@ -18,4 +18,11 @@ public class Stone {
     public void setLive(boolean live) {
         this.live = live;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        return ((Stone) obj).getColor() == color;
+    }
 }
