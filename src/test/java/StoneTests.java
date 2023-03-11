a51a9235a769716068d7d692b5a64e5d41888d48
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-11 20:49:25 +0100

    Changed from assert to Assertion.False/True

--- src/test/java/StoneTests.java
+++ src/test/java/StoneTests.java
@@ -1,23 +1,23 @@
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class StoneTests {
 
     Stone stone = new Stone(Color.BLACK);
 
     @Test
     void testColorGetter() {
-        assert(stone.getColor() == Color.BLACK);
+        assertEquals(stone.getColor(), Color.BLACK);
     }
 
     @Test
     void testIsLiveAfterStoneCreation() {
-        assert(!stone.isLive());
+        assertFalse(stone.isLive());
     }
 
     @Test
     void testIsLiveAfterLiveSetter() {
         stone.setLive(true);
-        assert(stone.isLive());
+        assertTrue(stone.isLive());
     }
 
 }
