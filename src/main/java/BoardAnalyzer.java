d99864b4eca31e27a22d4d4d515952f64e2a1d58
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-16 22:58:29 +0100

    Added method that checks if the adjacent cells are occupied

--- src/main/java/BoardAnalyzer.java
+++ src/main/java/BoardAnalyzer.java
@@ -1,5 +1,8 @@
 
 
+import exceptions.InvalidPositionException;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 
 public class BoardAnalyzer {
@@ -17,6 +20,21 @@ public class BoardAnalyzer {
             if (numberOfFreeCellsOnBoard > 1) return true;
         }
         return false;
+    }
+
+    public boolean areAdjacentCellsOccupied(@NotNull Position cellPosition) {
+        for(int i = -1; i <= 1; i++) {
+            for(int j = -1; j <= 1; j++) {
+                try {
+                    if (j == 0 && i == 0) continue;
+                    Position adjacentCellPosition = new Position(cellPosition.getRow() + i, cellPosition.getColumn() + j);
+                    Cell adjacentCell = board.getCell(adjacentCellPosition);
+                    if (!adjacentCell.isOccupied()) return false;
+                } catch (InvalidPositionException ignored) {
+                }
+            }
+        }
+        return true;
     }
 
 }
--- src/test/java/BoardAnalyzerTests.java
+++ src/test/java/BoardAnalyzerTests.java
@@ -23,5 +23,14 @@ public class BoardAnalyzerTests {
             boardCell.getValue().putStone(new Stone(Stone.Color.WHITE));
         }
     }
+    @Test
+    void testAreAdjacentCellsOccupied() {
+        BoardAnalyzer boardAnalyzer = new BoardAnalyzer(board);
+        fillTheBoardWithWhiteStones();
+        Assertions.assertTrue(boardAnalyzer.areAdjacentCellsOccupied(new Position(1,1)));
+        board.getCell(new Position(1,1)).clear();
+        Assertions.assertTrue(boardAnalyzer.areAdjacentCellsOccupied(new Position(1,1)));
+        Assertions.assertFalse(boardAnalyzer.areAdjacentCellsOccupied(new Position(1,2)));
+    }
 
 }
