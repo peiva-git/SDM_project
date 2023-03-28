99c76d5e6f37b147688dccf45fc5c46799ae3771
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-29 01:45:30 +0200

    Added parametrized tests

--- src/test/java/FreedomPointsCounterTests.java
+++ src/test/java/FreedomPointsCounterTests.java
@@ -1,8 +1,12 @@
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -47,53 +51,77 @@ public class FreedomPointsCounterTests {
         assertEquals(printedBoard, board.toString());
     }
 
+    private static @NotNull Stream<Arguments> provideBoards() {
+        return Stream.of(
+                Arguments.of("4 W  B  W  B\n"
+                                + "3 W  B  W  B\n"
+                                + "2 W  B  W  B\n"
+                                + "1 W  B  W  B\n"
+                                + "  A  B  C  D",
+                        4, 4, 2, 2
+                ),
+                Arguments.of("4 W  W  W  W\n"
+                                + "3 B  B  B  B\n"
+                                + "2 W  W  W  W\n"
+                                + "1 B  B  B  B\n"
+                                + "  A  B  C  D",
+                        4, 4, 2, 2
+                ),
+                Arguments.of("4 W  -  -  B\n"
+                                + "3 -  W  B  -\n"
+                                + "2 -  B  W  -\n"
+                                + "1 B  -  -  W\n"
+                                + "  A  B  C  D",
+                        4, 4, 1, 1
+                ),
+                Arguments.of(
+                        "8 W  W  W  W  W  W  W  W\n"
+                                + "7 W  B  W  W  W  W  W  W\n"
+                                + "6 W  B  B  W  B  W  B  B\n"
+                                + "5 W  B  B  W  B  W  B  W\n"
+                                + "4 W  W  B  W  B  W  B  W\n"
+                                + "3 B  W  W  W  B  W  B  W\n"
+                                + "2 B  W  W  W  W  W  W  W\n"
+                                + "1 B  W  W  B  W  W  W  W\n"
+                                + "  A  B  C  D  E  F  G  H",
+                        8, 8, 2, 2
+                ),
+                Arguments.of(
+                        "8 W  W  W  W  W  W  W  W\n"
+                                + "7 W  B  W  W  W  W  W  W\n"
+                                + "6 W  B  B  W  B  W  B  B\n"
+                                + "5 W  B  B  W  B  W  B  W\n"
+                                + "4 W  W  B  W  B  W  B  W\n"
+                                + "3 B  W  W  B  B  W  B  W\n"
+                                + "2 B  W  W  W  B  W  W  W\n"
+                                + "1 B  W  W  W  W  W  W  W\n"
+                                + "  A  B  C  D  E  F  G  H",
+                        8, 8, 2, 2
+                ),
+                Arguments.of(
+                        "8 W  W  W  W  W  W  W  W\n"
+                                + "7 W  B  W  W  W  W  W  W\n"
+                                + "6 W  B  B  W  B  W  B  B\n"
+                                + "5 W  B  B  W  B  W  B  W\n"
+                                + "4 W  W  B  B  B  W  B  W\n"
+                                + "3 B  -  W  B  B  W  B  W\n"
+                                + "2 B  W  W  W  B  W  W  W\n"
+                                + "1 B  W  W  W  W  W  W  W\n"
+                                + "  A  B  C  D  E  F  G  H",
+                        8, 8, 3, 1
+                )
+        );
+    }
 
-    @Test
-    void testGetWinner() {
-        String printedBoard =
-                "8 W  W  W  W  W  W  W  W\n"
-                        + "7 W  B  W  W  W  W  W  W\n"
-                        + "6 W  B  B  W  B  W  B  B\n"
-                        + "5 W  B  B  W  B  W  B  W\n"
-                        + "4 W  W  B  W  B  W  B  W\n"
-                        + "3 B  W  W  W  B  W  B  W\n"
-                        + "2 B  W  W  W  W  W  W  W\n"
-                        + "1 B  W  W  B  W  W  W  W\n"
-                        + "  A  B  C  D  E  F  G  H";
 
-        Board board = parseBoardFromString(printedBoard, 8, 8);
+    @ParameterizedTest
+    @MethodSource("provideBoards")
+    void testGetWinner(String printedBoard, int numberOfRows, int numberOfColumns, int blackScore, int whiteScore) {
+        Board board = parseBoardFromString(printedBoard, numberOfRows, numberOfColumns);
         FreedomPointsCounter freedomPointsCounter = new FreedomPointsCounter(board);
         freedomPointsCounter.count();
-        Assertions.assertEquals(freedomPointsCounter.getBlackPlayerScore(),2);
-        Assertions.assertEquals(freedomPointsCounter.getWhitePlayerScore(), 2);
-        printedBoard =
-                "8 W  W  W  W  W  W  W  W\n"
-                        + "7 W  B  W  W  W  W  W  W\n"
-                        + "6 W  B  B  W  B  W  B  B\n"
-                        + "5 W  B  B  W  B  W  B  W\n"
-                        + "4 W  W  B  W  B  W  B  W\n"
-                        + "3 B  W  W  B  B  W  B  W\n"
-                        + "2 B  W  W  W  B  W  W  W\n"
-                        + "1 B  W  W  W  W  W  W  W\n"
-                        + "  A  B  C  D  E  F  G  H";
-        freedomPointsCounter.setBoard(parseBoardFromString(printedBoard, 8, 8));
-        freedomPointsCounter.count();
-        Assertions.assertEquals(freedomPointsCounter.getBlackPlayerScore(),2);
-        Assertions.assertEquals(freedomPointsCounter.getWhitePlayerScore(), 2);
-        printedBoard =
-                "8 W  W  W  W  W  W  W  W\n"
-                        + "7 W  B  W  W  W  W  W  W\n"
-                        + "6 W  B  B  W  B  W  B  B\n"
-                        + "5 W  B  B  W  B  W  B  W\n"
-                        + "4 W  W  B  B  B  W  B  W\n"
-                        + "3 B  -  W  B  B  W  B  W\n"
-                        + "2 B  W  W  W  B  W  W  W\n"
-                        + "1 B  W  W  W  W  W  W  W\n"
-                        + "  A  B  C  D  E  F  G  H";
-        freedomPointsCounter.setBoard(parseBoardFromString(printedBoard, 8, 8));
-        freedomPointsCounter.count();
-        Assertions.assertEquals(freedomPointsCounter.getBlackPlayerScore(),3);
-        Assertions.assertEquals(freedomPointsCounter.getWhitePlayerScore(), 1);
+        Assertions.assertEquals(freedomPointsCounter.getBlackPlayerScore(), blackScore);
+        Assertions.assertEquals(freedomPointsCounter.getWhitePlayerScore(), whiteScore);
     }
 
 }
