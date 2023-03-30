4dce69760ada2455e4e105a3695f9d066bcb1e0a
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-03-30 15:36:05 +0200

    Added Board interface

---
+++ src/main/java/Board.java
@@ -0,0 +1,10 @@
+import org.jetbrains.annotations.NotNull;
+
+
+public interface Board {
+    int getNumberOfRows();
+    int getNumberOfColumns();
+    boolean isCellOccupied(@NotNull Position position);
+    void clearCell(@NotNull Position position);
+    void clearBoard();
+}
