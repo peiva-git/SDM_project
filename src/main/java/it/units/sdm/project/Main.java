daa48c5cbfe82f349b3c4491631dc963a6317448
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-03-30 18:02:35 +0200

    Refactor

--- src/main/java/it/units/sdm/project/Main.java
+++ src/main/java/it/units/sdm/project/Main.java
@@ -2,13 +2,14 @@ package it.units.sdm.project;
 
 import it.units.sdm.project.core.FreedomBoard;
 import it.units.sdm.project.core.FreedomGame;
+import it.units.sdm.project.interfaces.Game;
 
 public class Main {
     public static void main(String[] args) {
         Player white = new Player(Stone.Color.WHITE, "Mario", "Rossi");
         Player black = new Player(Stone.Color.BLACK, "Marco", "Neri");
         FreedomBoard board = new FreedomBoard(8, 8);
-        FreedomGame game = new FreedomGame(board, white, black);
+        Game game = new FreedomGame(board, white, black);
         game.start();
     }
 }
