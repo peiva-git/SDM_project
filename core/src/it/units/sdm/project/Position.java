e9574b2ccf78d8a91f9247fe3e4e24fc551dd296
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-04-06 13:00:15 +0200

    Added web application setup

--- core/src/it/units/sdm/project/Position.java
+++ core/src/it/units/sdm/project/Position.java
@@ -45,7 +45,7 @@ public class Position implements Comparable<Position> {
     @Override
     public String toString() {
         char column = (char) ('A' + this.column - 1);
-        return String.format("%c%d", column, row);
+        return column + " " + row;
     }
 
     /**
---
+++ html/src/it/units/sdm/project/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package it.units.sdm.project.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import it.units.sdm.project.FreedomGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new FreedomGame();
+        }
+}
