e3b80b8a4550a54494983b04e301ac3908ac182c
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-04-11 21:44:15 +0200

    Added multiple screens, following the template project at https://github.com/libgdx/libgdx.github.io/blob/master/assets/downloads/tutorials/extended-game-java/GameScreen.java

--- core/src/it/units/sdm/project/core/game/gui/FreedomGame.java
+++ core/src/it/units/sdm/project/core/game/gui/FreedomGame.java
@@ -1,99 +1,40 @@
 package it.units.sdm.project.core.game.gui;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.MapLayers;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import org.jetbrains.annotations.NotNull;
 
-public class FreedomGame extends ApplicationAdapter {
+public class FreedomGame extends Game {
 
-    private OrthographicCamera camera;
-    private OrthogonalTiledMapRenderer render;
-    private final static int width = 150, height = 100, layercount = 1;
-    private TiledMap map;
+    @NotNull
+    private SpriteBatch batch;
+    @NotNull
+    private BitmapFont font;
 
     @Override
     public void create() {
-
-        float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-
-        camera = new OrthographicCamera(w, h);
-
-        Pixmap blackSquare = new Pixmap(32, 32, Pixmap.Format.RGB565);
-        Pixmap whiteSquare = new Pixmap(32, 32, Pixmap.Format.RGB565);
-        whiteSquare.setColor(Color.WHITE);
-        whiteSquare.fillRectangle(0, 0, 32, 32);
-        blackSquare.setColor(Color.BLACK);
-        blackSquare.fillRectangle(0, 0, 32, 32);
-        TextureRegion blackTextureRegion = new TextureRegion(new Texture(blackSquare),0,0,32,32);
-        TextureRegion whiteTextureRegion = new TextureRegion(new Texture(whiteSquare),0,0,32,32);
-
-
-        map = new TiledMap();
-        MapLayers layers = map.getLayers();
-        for (int l = 0; l < layercount; l++) {
-            TiledMapTileLayer layer = new TiledMapTileLayer(width, height, 32,
-                    32);
-            for (int x = 0; x < width; x++) {
-                for (int y = 0; y < height; y++) {
-                    TiledMapTileLayer.Cell cell = new TiledMapTileLayer.Cell();
-                    if (y % 2 != 0) {
-                        if (x % 2 != 0) {
-                            cell.setTile(new StaticTiledMapTile(whiteTextureRegion));
-                        } else {
-                            cell.setTile(new StaticTiledMapTile(blackTextureRegion));
-                        }
-                    } else {
-                        if (x % 2 != 0) {
-                            cell.setTile(new StaticTiledMapTile(blackTextureRegion));
-                        } else {
-                            cell.setTile(new StaticTiledMapTile(whiteTextureRegion));
-                        }
-                    }
-                    layer.setCell(x, y, cell);
-                }
-            }
-            layers.add(layer);
-        }
-        render = new OrthogonalTiledMapRenderer(map);
-        render.setView(camera);
-        camera.translate((float) Gdx.graphics.getWidth() / 2,
-                (float) Gdx.graphics.getHeight() / 2);
-
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        setScreen(new MainMenuScreen(this));
     }
 
     @Override
     public void dispose() {
-        render.dispose();
-        map.dispose();
+        batch.dispose();
+        font.dispose();
     }
-
-    private static final float movmentspeed = 5f;
 
     @Override
     public void render() {
+        super.render();
+    }
 
-        Gdx.gl.glClearColor(1, 1, 1, 1);
-        Gdx.gl.glClear(GL20.GL_TEXTURE10);
-        camera.update();
-        render.setView(camera);
-        render.render();
+    public @NotNull SpriteBatch getBatch() {
+        return batch;
+    }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            camera.translate(-movmentspeed, 0);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            camera.translate(movmentspeed, 0);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            camera.translate(0, movmentspeed);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            camera.translate(0, -movmentspeed);
-        }
+    public @NotNull BitmapFont getFont() {
+        return font;
     }
 }
---
+++ core/src/it/units/sdm/project/core/game/gui/MainMenuScreen.java
@@ -0,0 +1,64 @@
+package it.units.sdm.project.core.game.gui;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+import org.jetbrains.annotations.NotNull;
+
+public class MainMenuScreen implements Screen {
+
+    private static final int VIEWPORT_WIDTH = 800;
+    private static final int VIEWPORT_HEIGHT = 400;
+    @NotNull
+    private final OrthographicCamera camera;
+    @NotNull
+    private final FreedomGame game;
+
+    public MainMenuScreen(final @NotNull FreedomGame game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+
+        camera.update();
+        game.getBatch().begin();
+        game.getFont().draw(game.getBatch(), "Welcome to th Freedom game!", 100, 150);
+        game.getFont().draw(game.getBatch(), "Tap anywhere to begin!", 100, 100);
+        game.getBatch().end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
