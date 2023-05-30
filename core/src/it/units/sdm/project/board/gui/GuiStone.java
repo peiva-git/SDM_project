88f806bea8f29e5628af1ebc37ef51c6cc210301
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-05-31 01:49:39 +0200

    Update

--- core/src/it/units/sdm/project/board/gui/GuiStone.java
+++ core/src/it/units/sdm/project/board/gui/GuiStone.java
@@ -1,6 +1,5 @@
 package it.units.sdm.project.board.gui;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
