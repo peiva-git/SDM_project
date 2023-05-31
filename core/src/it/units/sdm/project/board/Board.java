c125219b84eaa780828e11803f148601b4e61796
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-05-31 18:51:08 +0200

    Refactor

--- core/src/it/units/sdm/project/board/Board.java
+++ core/src/it/units/sdm/project/board/Board.java
@@ -12,17 +12,10 @@ public interface Board<P> {
     void putPiece(@NotNull P piece,@NotNull  Position position) throws InvalidPositionException;
     @Nullable P getPiece(@NotNull Position position) throws InvalidPositionException;
     @NotNull Set<Position> getPositions();
-    default long getNumberOfFreeCells() {
-        return getPositions().stream()
-                .filter(position -> !isCellOccupied(position))
-                .count();
-    }
     default boolean isCellOccupied(@NotNull Position position) throws InvalidPositionException {
         return getPiece(position) != null;
     }
-
     default void clearBoard() {
         getPositions().forEach(this::clearCell);
     }
-
 }
