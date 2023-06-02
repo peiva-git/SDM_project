2a6e59a15c8fe9bcb0f643caa0d1ba380dc9080c
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-03 00:15:21 +0200

    Added javadocs to BoardGame

--- core/src/it/units/sdm/project/game/BoardGame.java
+++ core/src/it/units/sdm/project/game/BoardGame.java
@@ -7,17 +7,50 @@ import org.jetbrains.annotations.Nullable;
 
 public interface BoardGame {
 
+    /**
+     *
+     * @return The board used by the game
+     */
     @NotNull Board<?> getBoard();
+
+    /**
+     *
+     * @return The player who's going first. In a game of chess, that would probably be the white player
+     */
     @NotNull Player getFirstPlayer();
+
+    /**
+     *
+     * @return The player who's going second. In a game of chess, that would probably be the black player
+     */
     @NotNull Player getSecondPlayer();
+
+    /**
+     *
+     * @return The last move played in this game
+     */
     @Nullable Move getLastMove();
+
+    /**
+     * Play the move in this game
+     * @param position The chosen position for the next move
+     */
     void nextMove(@NotNull Position position);
+
+    /**
+     *
+     * @return The player who should play next
+     */
     default @NotNull Player getNextPlayer() {
         Player firstPlayer = getFirstPlayer();
         Player secondPlayer = getSecondPlayer();
         if(getLastMove() == null || getLastMove().getPlayer().equals(secondPlayer)) return firstPlayer;
         return secondPlayer;
     }
+
+    /**
+     * Reset the game to the default state
+     */
     void reset();
 
 }
