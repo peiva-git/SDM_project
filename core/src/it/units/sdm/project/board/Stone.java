ac02e5557c4fc5384024bf05dca70090a5136d29
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-03 01:00:46 +0200

    Added javadocs to Stone

--- core/src/it/units/sdm/project/board/Stone.java
+++ core/src/it/units/sdm/project/board/Stone.java
@@ -7,10 +7,18 @@ public class Stone {
     @NotNull
     private final Color color;
 
+    /**
+     * Creates a new Stone instance to put on a Board
+     * @param color The stone's color. In a game of checkers, this would be black or white
+     */
     public Stone(@NotNull Color color) {
         this.color = color;
     }
 
+    /**
+     * Returns this stone's color
+     * @return This stone's color
+     */
     @NotNull
     public Color getColor() {
         return this.color;
