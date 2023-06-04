ed2457833674f4625551267016dc475d27fd598a
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-04 15:46:17 +0200

    Refactor package-info.java

--- core/src/it/units/sdm/project/board/gui/package-info.java
+++ core/src/it/units/sdm/project/board/gui/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains the GUI implementation of the {@link it.units.sdm.project.board.Board} interface by using
- * the <a href="https://libgdx.com/">libGDX</a> library.
+ * Contains the GUI implementation of the {@link it.units.sdm.project.board.Board} interface,
+ * written using the <a href="https://libgdx.com/">libGDX</a> library.
  */
 package it.units.sdm.project.board.gui;
