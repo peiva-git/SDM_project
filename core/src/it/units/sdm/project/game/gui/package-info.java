a7b60986aa1ed3934878829774aa9dcecb43bee4
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-04 16:02:01 +0200

    Refactor package-info.java

--- core/src/it/units/sdm/project/game/gui/package-info.java
+++ core/src/it/units/sdm/project/game/gui/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains the GUI implementation of the Freedom {@link it.units.sdm.project.game.BoardGame} by using
- * the <a href="https://libgdx.com/">libGDX</a> library.
+ * Contains the GUI implementation of the Freedom {@link it.units.sdm.project.game.BoardGame},
+ * written using the <a href="https://libgdx.com/">libGDX</a> library.
  */
 package it.units.sdm.project.game.gui;
