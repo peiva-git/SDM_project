a8aa76235debb6cc48c45509e99c9e65fd886496
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-04 15:55:33 +0200

    Refactor package-info.java

--- core/src/it/units/sdm/project/exceptions/package-info.java
+++ core/src/it/units/sdm/project/exceptions/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains all the custom {@link java.lang.RuntimeException} related to the {@link it.units.sdm.project.board.Board} interface
+ * Contains all the custom {@link java.lang.RuntimeException}s related to the {@link it.units.sdm.project.board.Board} interface
  * and to the {@link it.units.sdm.project.board.Position} class.
  */
 package it.units.sdm.project.exceptions;
