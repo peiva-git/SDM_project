5b46106192e1330a5ef37cb5bf8f163db2e0b367
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-05 15:59:07 +0200

    Added javadocs for NameSizeValidator

--- terminal/src/main/java/it/units/sdm/project/NameSizeValidator.java
+++ terminal/src/main/java/it/units/sdm/project/NameSizeValidator.java
@@ -4,8 +4,17 @@ import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.ParameterException;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This class is used to validate the {@link it.units.sdm.project.game.Player}'s name and surname size parameter
+ * in the terminal version of the game. These two parameters are given as command line arguments.
+ * The validation is done by implementing the {@link IParameterValidator} interface,
+ * which is part of the <a href="https://jcommander.org/">JCommander</a> project.
+ */
 public class NameSizeValidator implements IParameterValidator{
 
+    /**
+     * Maximum allowed {@link it.units.sdm.project.game.Player} name or surname size.
+     */
     public static final int MAX_NAME_LENGTH = 30;
 
     @Override
