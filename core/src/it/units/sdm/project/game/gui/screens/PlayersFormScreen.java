f027938d0dfea0b5a2fbd03dbcbe2d361bc0f6e8
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-12 14:13:29 +0200

    Exploring text field validation

--- core/src/it/units/sdm/project/game/gui/screens/PlayersFormScreen.java
+++ core/src/it/units/sdm/project/game/gui/screens/PlayersFormScreen.java
@@ -5,10 +5,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.util.form.SimpleFormValidator;
+import com.kotcrab.vis.ui.widget.VisValidatableTextField;
 import org.jetbrains.annotations.NotNull;
 
 import static it.units.sdm.project.game.gui.screens.GameScreen.GAME_SCREEN_WORLD_HEIGHT;
@@ -27,6 +31,17 @@ public class PlayersFormScreen implements Screen {
         skin = new Skin(Gdx.files.internal("UI/uiskin.json"));
         skin.addRegions(atlas);
         VisUI.load(skin);
+        Label whitePlayerName = new Label("First player's name", VisUI.getSkin());
+        Label whitePlayerSurname = new Label("First player's surname", VisUI.getSkin());
+        VisValidatableTextField whitePlayerNameText = new VisValidatableTextField(input -> {
+            if (input.isBlank() || input.isEmpty()) {
+                return false;
+            }
+            return true;
+        });
+        SimpleFormValidator form = new SimpleFormValidator(whitePlayerNameText, new Label("test", VisUI.getSkin()));
+        Table container = new Table();
+
     }
 
     @Override
