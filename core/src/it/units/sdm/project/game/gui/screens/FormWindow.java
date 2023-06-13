ca9d68cf5d270a7555b5a3b1f3eb8ce1a0c0e245
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-13 18:23:46 +0200

    Refactored to separate FormWindow class

---
+++ core/src/it/units/sdm/project/game/gui/screens/FormWindow.java
@@ -0,0 +1,138 @@
+package it.units.sdm.project.game.gui.screens;
+
+import com.badlogic.gdx.graphics.Color;
+import com.kotcrab.vis.ui.util.form.FormInputValidator;
+import com.kotcrab.vis.ui.util.form.SimpleFormValidator;
+import com.kotcrab.vis.ui.widget.*;
+import org.jetbrains.annotations.NotNull;
+
+import static it.units.sdm.project.board.MapBoard.MAX_BOARD_SIZE;
+import static it.units.sdm.project.board.MapBoard.MIN_BOARD_SIZE;
+
+public class FormWindow extends VisTable {
+    private static final int MAX_NAME_LENGTH = 20;
+    @NotNull
+    private final VisValidatableTextField whitePlayerName;
+    @NotNull
+    private final VisValidatableTextField whitePlayerSurname;
+    @NotNull
+    private final VisValidatableTextField blackPlayerName;
+    @NotNull
+    private final VisValidatableTextField blackPlayerSurname;
+    @NotNull
+    private final VisLabel errorMessage;
+    @NotNull
+    private final VisSlider boardSize;
+    @NotNull
+    private final VisTextField boardSizeText;
+    @NotNull
+    private final VisTextButton continueButton;
+
+    public FormWindow() {
+        continueButton = new VisTextButton("Let's play!");
+        whitePlayerName = new VisValidatableTextField();
+        whitePlayerSurname = new VisValidatableTextField();
+        blackPlayerName = new VisValidatableTextField();
+        blackPlayerSurname = new VisValidatableTextField();
+
+        whitePlayerName.setMaxLength(MAX_NAME_LENGTH + 1);
+        whitePlayerSurname.setMaxLength(MAX_NAME_LENGTH + 1);
+        blackPlayerName.setMaxLength(MAX_NAME_LENGTH + 1);
+        blackPlayerSurname.setMaxLength(MAX_NAME_LENGTH + 1);
+
+        errorMessage = new VisLabel();
+        errorMessage.setColor(Color.RED);
+
+        boardSize = new VisSlider(2, MAX_BOARD_SIZE, 1, false);
+        boardSizeText = new VisTextField(String.valueOf(MIN_BOARD_SIZE));
+        boardSizeText.setReadOnly(true);
+        boardSizeText.setDisabled(true);
+
+        defaults().padLeft(10);
+        defaults().padRight(10);
+        columnDefaults(0).left();
+        add(new VisLabel("First player's name: "));
+        add(whitePlayerName).expand().fill();
+        row();
+        add(new VisLabel("First player's surname: "));
+        add(whitePlayerSurname).expand().fill();
+        row();
+        add(new VisLabel("Second player's name: "));
+        add(blackPlayerName).expand().fill();
+        row();
+        add(new VisLabel("Second player's surname: "));
+        add(blackPlayerSurname).expand().fill();
+        row();
+        add(errorMessage).expand().fill().colspan(2);
+        row();
+        add(new VisLabel("Choose board size: "));
+        add(boardSizeText).expand().fill();
+        row();
+        add(boardSize).fill().expand().colspan(2);
+        row();
+        add(continueButton).fill().expand().colspan(2).padBottom(3);
+
+        SimpleFormValidator validator = new SimpleFormValidator(continueButton, errorMessage);
+        validator.setSuccessMessage("All good!");
+        validator.notEmpty(whitePlayerName, "First player's name can't be empty");
+        validator.custom(whitePlayerName, new FormInputValidator("First player's name length can't exceed " + MAX_NAME_LENGTH) {
+            @Override
+            protected boolean validate(String input) {
+                return input.length() <= MAX_NAME_LENGTH;
+            }
+        });
+        validator.notEmpty(whitePlayerSurname, "First player's surname can't be empty");
+        validator.custom(whitePlayerSurname, new FormInputValidator("First player's surname length can't exceed " + MAX_NAME_LENGTH) {
+            @Override
+            protected boolean validate(String input) {
+                return input.length() <= MAX_NAME_LENGTH;
+            }
+        });
+        validator.notEmpty(blackPlayerName, "Second player's name can't be empty");
+        validator.custom(blackPlayerName, new FormInputValidator("Second player's name length can't exceed " + MAX_NAME_LENGTH) {
+            @Override
+            protected boolean validate(String input) {
+                return input.length() <= MAX_NAME_LENGTH;
+            }
+        });
+        validator.notEmpty(blackPlayerSurname, "Second player's surname can't be empty");
+        validator.custom(blackPlayerSurname, new FormInputValidator("Second player's surname length can't exceed " + MAX_NAME_LENGTH) {
+            @Override
+            protected boolean validate(String input) {
+                return input.length() <= MAX_NAME_LENGTH;
+            }
+        });
+    }
+
+    public @NotNull VisValidatableTextField getWhitePlayerName() {
+        return whitePlayerName;
+    }
+
+    public @NotNull VisValidatableTextField getWhitePlayerSurname() {
+        return whitePlayerSurname;
+    }
+
+    public @NotNull VisValidatableTextField getBlackPlayerName() {
+        return blackPlayerName;
+    }
+
+    public @NotNull VisValidatableTextField getBlackPlayerSurname() {
+        return blackPlayerSurname;
+    }
+
+    public @NotNull VisLabel getErrorMessage() {
+        return errorMessage;
+    }
+
+    public @NotNull VisSlider getBoardSize() {
+        return boardSize;
+    }
+
+    public @NotNull VisTextField getBoardSizeText() {
+        return boardSizeText;
+    }
+
+    public @NotNull VisTextButton getContinueButton() {
+        return continueButton;
+    }
+}
--- core/src/it/units/sdm/project/game/gui/screens/PlayersFormScreen.java
+++ core/src/it/units/sdm/project/game/gui/screens/PlayersFormScreen.java
@@ -9,20 +9,14 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.kotcrab.vis.ui.VisUI;
-import com.kotcrab.vis.ui.util.form.FormInputValidator;
-import com.kotcrab.vis.ui.util.form.SimpleFormValidator;
 import com.kotcrab.vis.ui.widget.*;
 import it.units.sdm.project.game.Player;
 import it.units.sdm.project.game.gui.FreedomGame;
 import org.jetbrains.annotations.NotNull;
 
-import static it.units.sdm.project.board.MapBoard.MAX_BOARD_SIZE;
-import static it.units.sdm.project.board.MapBoard.MIN_BOARD_SIZE;
 import static it.units.sdm.project.board.gui.GuiBoard.TILE_SIZE;
 
 public class PlayersFormScreen implements Screen {
-
-    private static final int MAX_NAME_LENGTH = 20;
 
     @NotNull
     private final Stage stage;
@@ -33,100 +27,28 @@ public class PlayersFormScreen implements Screen {
         stage = new Stage(new FitViewport(FORM_SCREEN_WORLD_WIDTH, FORM_SCREEN_WORLD_HEIGHT));
         VisUI.load(VisUI.SkinScale.X2);
 
-        VisTextButton continueButton = new VisTextButton("Let's play!");
-        VisValidatableTextField whitePlayerName = new VisValidatableTextField();
-        VisValidatableTextField whitePlayerSurname = new VisValidatableTextField();
-        VisValidatableTextField blackPlayerName = new VisValidatableTextField();
-        VisValidatableTextField blackPlayerSurname = new VisValidatableTextField();
+        FormWindow form = new FormWindow();
+        form.setFillParent(true);
 
-        whitePlayerName.setMaxLength(MAX_NAME_LENGTH + 1);
-        whitePlayerSurname.setMaxLength(MAX_NAME_LENGTH + 1);
-        blackPlayerName.setMaxLength(MAX_NAME_LENGTH + 1);
-        blackPlayerSurname.setMaxLength(MAX_NAME_LENGTH + 1);
-
-        VisLabel errorMessage = new VisLabel();
-        errorMessage.setColor(Color.RED);
-
-        VisSlider boardSize = new VisSlider(2, MAX_BOARD_SIZE, 1, false);
-        VisTextField boardSizeText = new VisTextField(String.valueOf(MIN_BOARD_SIZE));
-        boardSizeText.setReadOnly(true);
-        boardSizeText.setDisabled(true);
-
-        VisTable container = new VisTable(true);
-        container.setFillParent(true);
-        container.padLeft(10).padRight(10);
-        container.columnDefaults(0).left();
-        container.add(new VisLabel("First player's name: "));
-        container.add(whitePlayerName).expand().fill();
-        container.row();
-        container.add(new VisLabel("First player's surname: "));
-        container.add(whitePlayerSurname).expand().fill();
-        container.row();
-        container.add(new VisLabel("Second player's name: "));
-        container.add(blackPlayerName).expand().fill();
-        container.row();
-        container.add(new VisLabel("Second player's surname: "));
-        container.add(blackPlayerSurname).expand().fill();
-        container.row();
-        container.add(errorMessage).expand().fill().colspan(2);
-        container.row();
-        container.add(new VisLabel("Choose board size: "));
-        container.add(boardSizeText).expand().fill();
-        container.row();
-        container.add(boardSize).fill().expand().colspan(2);
-        container.row();
-        container.add(continueButton).fill().expand().colspan(2).padBottom(3);
-
-        SimpleFormValidator validator = new SimpleFormValidator(continueButton, errorMessage);
-        validator.setSuccessMessage("All good!");
-        validator.notEmpty(whitePlayerName, "First player's name can't be empty");
-        validator.custom(whitePlayerName, new FormInputValidator("First player's name length can't exceed " + MAX_NAME_LENGTH) {
-            @Override
-            protected boolean validate(String input) {
-                return input.length() <= MAX_NAME_LENGTH;
-            }
-        });
-        validator.notEmpty(whitePlayerSurname, "First player's surname can't be empty");
-        validator.custom(whitePlayerSurname, new FormInputValidator("First player's surname length can't exceed " + MAX_NAME_LENGTH) {
-            @Override
-            protected boolean validate(String input) {
-                return input.length() <= MAX_NAME_LENGTH;
-            }
-        });
-        validator.notEmpty(blackPlayerName, "Second player's name can't be empty");
-        validator.custom(blackPlayerName, new FormInputValidator("Second player's name length can't exceed " + MAX_NAME_LENGTH) {
-            @Override
-            protected boolean validate(String input) {
-                return input.length() <= MAX_NAME_LENGTH;
-            }
-        });
-        validator.notEmpty(blackPlayerSurname, "Second player's surname can't be empty");
-        validator.custom(blackPlayerSurname, new FormInputValidator("Second player's surname length can't exceed " + MAX_NAME_LENGTH) {
-            @Override
-            protected boolean validate(String input) {
-                return input.length() <= MAX_NAME_LENGTH;
-            }
-        });
-
-        continueButton.addListener(new ChangeListener() {
+        form.getContinueButton().addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                game.setWhitePlayer(new Player(Color.WHITE, whitePlayerName.getText(), whitePlayerSurname.getText()));
-                game.setBlackPlayer(new Player(Color.BLACK, blackPlayerName.getText(), blackPlayerSurname.getText()));
-                game.setNumberOfRowsAndColumns((int) boardSize.getValue());
+                game.setWhitePlayer(new Player(Color.WHITE, form.getWhitePlayerName().getText(), form.getWhitePlayerSurname().getText()));
+                game.setBlackPlayer(new Player(Color.BLACK, form.getBlackPlayerName().getText(), form.getBlackPlayerSurname().getText()));
+                game.setNumberOfRowsAndColumns((int) form.getBoardSize().getValue());
                 game.setScreen(new GameScreen(game));
                 dispose();
             }
         });
 
-        boardSize.addListener(new ChangeListener() {
+        form.getBoardSize().addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                boardSizeText.setText(String.format("%d", (int) ((VisSlider) actor).getValue()));
+                form.getBoardSizeText().setText(String.format("%d", (int) ((VisSlider) actor).getValue()));
             }
         });
 
-        stage.addActor(container);
+        stage.addActor(form);
         Gdx.input.setInputProcessor(stage);
     }
 
