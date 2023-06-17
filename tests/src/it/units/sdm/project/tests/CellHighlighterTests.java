68ae1c71f0d07f1c42f1000841d7f99eb25e41d2
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-17 20:21:39 +0200

    Got rid of unnecessary dependencies

--- tests/src/it/units/sdm/project/tests/CellHighlighterTests.java
+++ tests/src/it/units/sdm/project/tests/CellHighlighterTests.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.backends.headless.HeadlessFiles;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
