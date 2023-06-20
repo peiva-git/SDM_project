a1c5ff6fdac04350be221f302d038f7a84d24cbb
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-20 10:41:57 +0200

    Added GuiStoneTests getter test

--- core/test/java/board/GuiStoneTests.java
+++ core/test/java/board/GuiStoneTests.java
@@ -4,15 +4,16 @@ import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.kotcrab.vis.ui.VisUI;
+import it.units.sdm.project.board.Piece;
 import it.units.sdm.project.board.gui.GuiStone;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import utility.FreedomHeadlessApplicationUtils;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GuiStoneTests {
 
@@ -32,6 +33,13 @@ public class GuiStoneTests {
         } else {
             assertThrows(expectedException, () -> new GuiStone(stoneColor, VisUI.getSkin().get(WHITE_STONE_IMAGE_NAME, TextureRegion.class)));
         }
+    }
+
+    @Test
+    void testColorGetter() {
+        Piece blackStone = new GuiStone(Color.BLACK, VisUI.getSkin().get(WHITE_STONE_IMAGE_NAME, TextureRegion.class));
+        assertEquals(Color.BLACK, blackStone.getPieceColor());
+        assertNotEquals(Color.WHITE, blackStone.getPieceColor());
     }
 
     @AfterAll
