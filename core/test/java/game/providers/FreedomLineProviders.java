6a50e3f9e0404ecbdd12c856487cf74efe3eb0b8
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-06-21 01:35:16 +0200

    Update

--- core/test/java/game/providers/FreedomLineProviders.java
+++ core/test/java/game/providers/FreedomLineProviders.java
@@ -29,66 +29,66 @@ public class FreedomLineProviders {
 
     public static @NotNull Stream<Arguments> provideSetOfPositions() {
         return Stream.of(
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(7, 0),
-//                                Position.fromCoordinates(6, 0),
-//                                Position.fromCoordinates(5, 0),
-//                                Position.fromCoordinates(4, 0),
-//                                Position.fromCoordinates(3, 0)
-//                        )),
-//                        null
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(7, 0),
-//                                Position.fromCoordinates(6, 0),
-//                                Position.fromCoordinates(5, 0),
-//                                Position.fromCoordinates(4, 0),
-//                                Position.fromCoordinates(3, 0),
-//                                Position.fromCoordinates(2, 0)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(7, 0),
-//                                Position.fromCoordinates(6, 1)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(6, 1),
-//                                Position.fromCoordinates(5, 2),
-//                                Position.fromCoordinates(4, 2)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(0, 1),
-//                                Position.fromCoordinates(1, 2),
-//                                Position.fromCoordinates(2, 3),
-//                                Position.fromCoordinates(3, 3)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(7, 0),
-//                                Position.fromCoordinates(6, 2)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
-//                Arguments.of(
-//                        new LinkedHashSet<>(List.of(
-//                                Position.fromCoordinates(0, 1),
-//                                Position.fromCoordinates(0, 2),
-//                                Position.fromCoordinates(1, 2)
-//                        )),
-//                        InvalidPositionException.class
-//                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(7, 0),
+                                Position.fromCoordinates(6, 0),
+                                Position.fromCoordinates(5, 0),
+                                Position.fromCoordinates(4, 0),
+                                Position.fromCoordinates(3, 0)
+                        )),
+                        null
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(7, 0),
+                                Position.fromCoordinates(6, 0),
+                                Position.fromCoordinates(5, 0),
+                                Position.fromCoordinates(4, 0),
+                                Position.fromCoordinates(3, 0),
+                                Position.fromCoordinates(2, 0)
+                        )),
+                        InvalidPositionException.class
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(7, 0),
+                                Position.fromCoordinates(6, 1)
+                        )),
+                        InvalidPositionException.class
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(6, 1),
+                                Position.fromCoordinates(5, 2),
+                                Position.fromCoordinates(4, 2)
+                        )),
+                        InvalidPositionException.class
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(0, 1),
+                                Position.fromCoordinates(1, 2),
+                                Position.fromCoordinates(2, 3),
+                                Position.fromCoordinates(3, 3)
+                        )),
+                        InvalidPositionException.class
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(7, 0),
+                                Position.fromCoordinates(6, 2)
+                        )),
+                        InvalidPositionException.class
+                ),
+                Arguments.of(
+                        new LinkedHashSet<>(List.of(
+                                Position.fromCoordinates(0, 1),
+                                Position.fromCoordinates(0, 2),
+                                Position.fromCoordinates(1, 2)
+                        )),
+                        InvalidPositionException.class
+                ),
                 Arguments.of(
                         new LinkedHashSet<>(List.of(
                                 Position.fromCoordinates(0, 1),
