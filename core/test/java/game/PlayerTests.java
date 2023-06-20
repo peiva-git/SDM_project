3ed780fc07b89b837cbd2c22c0e4397fb280d8e2
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-06-20 11:57:23 +0200

    Added equals tests

--- core/test/java/game/PlayerTests.java
+++ core/test/java/game/PlayerTests.java
@@ -9,11 +9,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTests {
 
     @Test
-    void testPlayerEquals() {
+    void testEqualsForTwoPlayersWithDifferentColorsAndDifferentUsername() {
+        Player blackPlayer = new Player(Color.BLACK, "black");
+        Player whitePlayer = new Player(Color.WHITE, "white");
+        assertNotEquals(whitePlayer, blackPlayer);
+        assertNotEquals(blackPlayer, whitePlayer);
+    }
+
+    @Test
+    void testEqualsForTwoPlayersWithDifferentColorsAndSameUsername() {
         Player blackPlayer = new Player(Color.BLACK, "username");
         Player whitePlayer = new Player(Color.WHITE, "username");
+        assertNotEquals(whitePlayer, blackPlayer);
         assertNotEquals(blackPlayer, whitePlayer);
-        assertEquals(blackPlayer, blackPlayer);
+    }
+
+    @Test
+    void testEqualsForTwoPlayersWithSameColorAndSameUsername() {
+        Player firstBlackPlayer = new Player(Color.BLACK, "username");
+        Player secondBlackPlayer = new Player(Color.BLACK, "username");
+        assertEquals(secondBlackPlayer, firstBlackPlayer);
+        assertEquals(firstBlackPlayer, secondBlackPlayer);
+    }
+    @Test
+    void testEqualsWithOnePlayerAndNullValue() {
+        Player blackPlayer = new Player(Color.BLACK, "black");
+        assertNotEquals(blackPlayer, null);
+    }
+    @Test
+    void testEqualsForTwoPlayersWithSameColorAndDifferentUsername() {
+        Player firstBlackPlayer = new Player(Color.BLACK, "first_player");
+        Player secondBlackPlayer = new Player(Color.BLACK, "second_player");
+        assertEquals(secondBlackPlayer, firstBlackPlayer);
+        assertEquals(firstBlackPlayer, secondBlackPlayer);
     }
 
     @Test
