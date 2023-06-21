18aa39b2b7a3297e0bbf34a298c46bda0e2da544
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-21 19:06:11 +0200

    Added board size validation tests

---
+++ terminal/test/java/BoardSizeValidatorTests.java
@@ -0,0 +1,24 @@
+import com.beust.jcommander.ParameterException;
+import it.units.sdm.project.BoardSizeValidator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class BoardSizeValidatorTests {
+
+    private final BoardSizeValidator validator = new BoardSizeValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "5", "26"})
+    void testBoardSizeValidationWithCorrectValues(String inputValue) {
+        Assertions.assertDoesNotThrow(() -> validator.validate("White player username", inputValue));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"1", "27", "text"})
+    void testBoardSizeValidatorWithIncorrectValues(String inputValue) {
+        Assertions.assertThrows(ParameterException.class, () -> validator.validate("White player username", inputValue));
+    }
+}
