48987eb3364617a4d9cdad9f39bd0c5e43bbe5a3
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-06-21 17:38:51 +0200

    Removed code smell

--- core/test/java/game/FreedomBoardStatusObserverTests.java
+++ core/test/java/game/FreedomBoardStatusObserverTests.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static board.providers.BoardProviders.parseBoardFromString;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FreedomBoardStatusObserverTests {
+class FreedomBoardStatusObserverTests {
 
     @ParameterizedTest
     @MethodSource("game.providers.FreedomPointsCounterProviders#printedBoardsProvider")
