53bdd26f0526fc60c092bb14991a2542e7163cb1
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-06-21 21:04:45 +0200

    Class generalized to any board game

--- core/src/it/units/sdm/project/board/gui/TileClickListener.java
+++ core/src/it/units/sdm/project/board/gui/TileClickListener.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import it.units.sdm.project.board.Position;
+import it.units.sdm.project.game.BoardGame;
 import it.units.sdm.project.game.gui.FreedomGame;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,16 +14,16 @@ import org.jetbrains.annotations.NotNull;
  */
 public class TileClickListener extends ClickListener {
     @NotNull
-    private final FreedomGame game;
+    private final BoardGame<?> game;
 
     /**
      * Creates a new listener to be used with a {@link it.units.sdm.project.board.Board}. By default,
      * the listener will simply execute the {@link FreedomGame#nextMove(Position)} method from the {@link FreedomGame}
      * interface using the clicked tile's {@link Position}.
      * If a different behaviour is expected, the {@link ClickListener#clicked(InputEvent, float, float)} method should be overridden.
-     * @param game The {@link FreedomGame} instance on which to listen for tile click events
+     * @param game The {@link BoardGame} instance on which to listen for tile click events
      */
-    public TileClickListener(@NotNull FreedomGame game) {
+    public TileClickListener(@NotNull BoardGame<?> game) {
         this.game = game;
     }
 
@@ -34,7 +35,7 @@ public class TileClickListener extends ClickListener {
      */
     @Override
     public void clicked(@NotNull InputEvent event, float x, float y) {
-        GuiBoard<GuiStone> board = (GuiBoard<GuiStone>) game.getBoard();
+        GuiBoard<?> board = (GuiBoard<?>) game.getBoard();
         Cell<Actor> cell = board.getCell(event.getListenerActor());
         game.nextMove(board.fromTileCoordinatesToBoardPosition(cell.getRow(), cell.getColumn()));
         super.clicked(event, x, y);
