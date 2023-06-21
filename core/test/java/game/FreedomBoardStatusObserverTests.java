ef1319bc09049e985f70c6db53c8a46ee3353363
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-06-21 10:47:10 +0200

    Added test for getCurrentWinner and refactoring

--- core/test/java/game/FreedomBoardStatusObserverTests.java
+++ core/test/java/game/FreedomBoardStatusObserverTests.java
@@ -1,5 +1,6 @@
 package game;
 
+import com.badlogic.gdx.graphics.Color;
 import it.units.sdm.project.game.FreedomBoardStatusObserver;
 import it.units.sdm.project.game.Move;
 import org.jetbrains.annotations.NotNull;
@@ -7,10 +8,25 @@ import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.awt.*;
+
 import static board.providers.BoardProviders.parseBoardFromString;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FreedomBoardStatusObserverTests {
+
+    @ParameterizedTest
+    @MethodSource("game.providers.FreedomPointsCounterProviders#printedBoardsProvider")
+    void testCurrentWinner(String printedBoard, int boardSize, int blackScore, int whiteScore) {
+        FreedomBoardStatusObserver freedomBoardStatusObserver = new FreedomBoardStatusObserver(parseBoardFromString(printedBoard, boardSize));
+        if(blackScore > whiteScore) {
+            assertEquals(Color.BLACK, freedomBoardStatusObserver.getCurrentWinner());
+        } else if (blackScore < whiteScore) {
+            assertEquals(Color.WHITE, freedomBoardStatusObserver.getCurrentWinner());
+        } else {
+            assertNull(freedomBoardStatusObserver.getCurrentWinner());
+        }
+    }
 
     @ParameterizedTest
     @MethodSource("game.providers.FreedomBoardStatusObserverProvider#provide4x4BoardWithLastMovedPlayed")
